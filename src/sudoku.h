abe30f9e82b9a72403ecfd087ec1e7c0aae436d9
Federico Jure <fjure@atixlabs.com>
2019-09-05 18:45:40 -0300

    avanzo con api sudoku

--- src/sudoku.h
+++ src/sudoku.h
@@ -9,5 +9,9 @@ typedef struct {
 
 int sudoku_init(sudoku_t* self);
 int sudoku_release(sudoku_t* self);
+int sudoku_draw(sudoku_t* self, char* buffer);
+int sudoku_put_in_position(sudoku_t* self, int value, int row, int cloumn);
+int sudoku_clean(sudoku_t* self);
+int sudoku_verify(sudoku_t* self, int* win);
 
 #endif
